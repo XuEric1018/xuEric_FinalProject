265bdd0dc605c51e4bf09c0ca39aeb010ccd43d8
xueri <xueric1018@gmail.com>
2024-04-12 14:17:15 -0500

    imported template

---
+++ src/project.py
@@ -0,0 +1,22 @@
+
+    
+
+#class SomeClass():
+
+    #def method_1():
+        
+
+    #def method_2():
+        
+
+    #def method_n():
+        
+
+
+#def main():
+
+
+
+
+if __name__ == "__main__":
+    main()
