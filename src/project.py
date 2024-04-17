0687bad45656600a026e3c8dbd8895c7f329a7a4
xueri <xueric1018@gmail.com>
2024-04-16 19:31:06 -0500

    load image into class

--- src/project.py
+++ src/project.py
@@ -1,22 +1,25 @@
-
+import os
+from PIL import Image
+from PIL import ImageFilter
     
-
-#class SomeClass():
-
-    #def method_1():
-        
-
-    #def method_2():
-        
-
-    #def method_n():
-        
-
-
-#def main():
+class TurnMosaic:
+    def __init__ (self, image_name, mosaic_size):
+        self.image_name = image_name
+        self.mosaic_size = mosaic_size
+    
+    def load_image(self):
+        try:
+            self.image = Image.open(self.image_name)
+            return True
+        except FileNotFoundError:
+            print(f"Error: Image '{self.image_name}' not found.")
+            return False
 
 
 
+def main():
+    image_name = "LakeView.png"
+    mosaic_size = 50  
 
 if __name__ == "__main__":
     main()
